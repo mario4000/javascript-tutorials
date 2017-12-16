c9d62618bb35d711d28b0c42eab1affc222fc86f
christopher villanueva <christophervillanueva@christophers-MacBook-Pro.local>
2017-12-15 21:55:49 -0600

    add: static form and list ui; preview for components

--- reactReduxTodoList/src/App.js
+++ reactReduxTodoList/src/App.js
@@ -1,3 +1,10 @@
+/**
+* Update Type:
+* ============
+* adding static ui components and initial styles.
+* preview page then decide how to break up components
+*/
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -7,12 +14,39 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={logo}
+               className="App-logo"
+               alt="logo" />
+          <h1 className="App-title">
+            Welcome to React and Redux
+          </h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="Todo-App">
+
+            <form>
+              <input type="text" />
+            </form>
+
+            <div className="Todo-List">
+                <ul>
+                    <li>
+                      <input type="checkbox" />
+                      Create Static UI
+                    </li>
+                    <li>
+                      <input type="checkbox" />
+                      Create Initial State
+                    </li>
+                    <li>
+                      <input type="checkbox" />
+                      Bind State to Render UI
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+
       </div>
     );
   }
